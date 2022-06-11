d4ffa92a0da701dcc1a8cb4cac11f65f0896d714
Jun Kim <rlafbf222@naver.com>
2022-06-11 17:55:51 +0900

    게시글 CRUD (Security 전)

--- src/main/java/com/coffee/miniproject/model/Post.java
+++ src/main/java/com/coffee/miniproject/model/Post.java
@@ -1,5 +1,7 @@
 package com.coffee.miniproject.model;
 
+import com.coffee.miniproject.dto.PostRequestDto;
+import com.coffee.miniproject.dto.PostRequestDto4Put;
 import com.coffee.miniproject.util.Timestamped;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,8 +24,15 @@ public class Post extends Timestamped {
     @Column(nullable = false)
     private String contents;
 
+    // ERD 누락된 column!
     @Column(nullable = false)
-    private String imgPath;
+    private String nickname;
+
+    @Column(nullable = false)
+    private PostCategory category;
+
+    @Column()
+    private String img;
 
     // FK로 memberId 들어옴.
     @ManyToOne
@@ -33,4 +42,24 @@ public class Post extends Timestamped {
     // 자바 객체 사이드에서만 저장됨 (DB에 저장안됨)
     @OneToMany(mappedBy = "post")
     private List<Comment> comments;
+
+    // postRequestDto 받는 생성자
+    public Post(PostRequestDto requestDto, String nickname){
+        this.title = requestDto.getTitle();
+        this.contents = requestDto.getContents();
+        this.nickname = nickname;
+        this.img = requestDto.getImg();
+
+        if(requestDto.isCategory()){
+            this.category = PostCategory.RECIPE;
+        }else{
+            this.category = PostCategory.CAFE;
+        }
+
+    }
+
+    public void updatePost(PostRequestDto4Put requestDto) {
+        this.title = requestDto.getTitle();
+        this.contents = requestDto.getContents();
+    }
 }
