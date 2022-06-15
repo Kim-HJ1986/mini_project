a42a89e1b437ddc5685ca5453f86cd5f54154872
Jun Kim <rlafbf222@naver.com>
2022-06-15 21:36:35 +0900

    게시글 리턴값에 username 추가

--- src/main/java/com/coffee/miniproject/dto/PostResponseDto.java
+++ src/main/java/com/coffee/miniproject/dto/PostResponseDto.java
@@ -13,6 +13,8 @@ import java.time.LocalDateTime;
 public class PostResponseDto {
     private Long id;
 
+    private String username;
+
     private String title;
 
     private String contents;
@@ -28,6 +30,7 @@ public class PostResponseDto {
 
     public PostResponseDto(Post post){
         this.id = post.getId();
+        this.username = post.getMember().getUsername();
         this.title = post.getTitle();
         this.contents = post.getContents();
         this.nickname = post.getNickname();
